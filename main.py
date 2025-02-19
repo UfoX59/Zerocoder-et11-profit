77ea25270b526800449680579025c999f230c2ab
UfoX59 <alexd59@mail.ru>
2025-02-19 22:15:07 +0500

    Вычисляет была ли сделка прибыльной

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ def profit_calculation (c, p):
     prof = p - c
     return prof
 
+def is_profit (c, p):
+    prof = p -c
+    if prof > 0 :
+        return True
+    else:
+        return False
+
+
 cost = 77
 price = 100
 
 profit = profit_calculation(cost, price)
 print(f"Прибыль составляет {profit}")
 
+if is_profit(cost, price):
+    print ("Удачная сделка, есть прибыль")
+else:
+    print("Не повезло, сделка убыточна")
